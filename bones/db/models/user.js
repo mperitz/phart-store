79c761b5288af6e9051bb8c691ac4d260f44ed71
Robert Schroeder <robschroeder333@users.noreply.github.com>
2017-03-01 14:50:13 -0500

    defined user model closes #12

--- bones/db/models/user.js
+++ bones/db/models/user.js
@@ -6,20 +6,73 @@ const bcrypt = require('bcryptjs')
 const Sequelize = require('sequelize')
 const db = require('APP/db')
 
-const User = db.define('users', {
-  name: Sequelize.STRING,
+const User = db.define('users',
+{
+  // name: Sequelize.STRING,
   email: {
     type: Sequelize.STRING,
     validate: {
 			isEmail: true,
 			notEmpty: true,
-		}
+		},
+    allowNull: false
   },
+
+  //Added properties (begin)
+
+  username: {
+    type: Sequelize.STRING(25),
+    allowNull: false
+  },
+
+  profile_photo: Sequelize.STRING,
+
+  about_me: Sequelize.TEXT,
+
+  is_vendor: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
+
+  is_admin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
+
+  endorsed_by: {
+    type: Sequelize.ARRAY(Sequelize.INTEGER),
+    defaultValue: []
+  },
+
+  favorite_users: {
+    type: Sequelize.ARRAY(Sequelize.INTEGER),
+    validate: {
+      maxLength: function(val) {
+        const favoritesLimit = 3
+        if (val.length > favoritesLimit) throw new Error(`user can only have ${favoritesLimit}`)
+      }
+    },
+    defaultValue: []
+  },
+
+  favorite_bands: {
+    type: Sequelize.ARRAY(Sequelize.INTEGER),
+    validate: {
+      maxLength: function(val) {
+        const favoritesLimit = 3
+        if (val.length > favoritesLimit) throw new Error(`user can only have ${favoritesLimit}`)
+      }
+    },
+    defaultValue: []
+  },
+  //Added poperties (ends)
 
   // We support oauth, so users may or may not have passwords.
   password_digest: Sequelize.STRING, // This column stores the hashed password in the DB, via the beforeCreate/beforeUpdate hooks
 	password: Sequelize.VIRTUAL // Note that this is a virtual, and not actually stored in DB
-}, {
+},
+
+{
 	indexes: [{fields: ['email'], unique: true}],
   hooks: {
     beforeCreate: setEmailAndPassword,
@@ -34,7 +87,18 @@ const User = db.define('users', {
           else resolve(result)
         })
       )
-    }
+    },
+
+    // updateFavorites (whichFavorites, newFavoritesArray) {
+
+    //   if (Array.isArray(newFavoritesArray)) {
+    //     if (whichFavorites === 'users') {
+    //       this.favoriteUsers = newFavoritesArray
+    //     } else if (whichFavorites === 'bands') {
+    //       this.favoriteBands = newFavoritesArray
+    //     }
+    //   }
+    // }
   }
 })
 
--- bones/db/models/user.test.js
+++ bones/db/models/user.test.js
@@ -4,10 +4,12 @@ const db = require('APP/db')
 const User = require('./user')
 const {expect} = require('chai')
 
-describe('User', () => {
+describe('User oAuth', () => {
   before('wait for the db', () => db.didSync)
 
+
   describe('authenticate(plaintext: String) ~> Boolean', () => {
+
     it('resolves true if the password matches', () =>
       User.create({ password: 'ok' })
         .then(user => user.authenticate('ok'))
@@ -17,5 +19,146 @@ describe('User', () => {
       User.create({ password: 'ok' })
         .then(user => user.authenticate('not ok'))
         .then(result => expect(result).to.be.false))
+  })
+
+
+
+})
+
+describe('User', () => {
+  let user1, user2
+
+  beforeEach('add user samples', () => {
+    user1 = User.build({
+      username: 'correctUser',
+      email: 'phish@fish.com',
+      is_vendor: true,
+      is_admin: true,
+      endorsed_by: [21, 4, 3, 7, 'fish'],
+      favorite_users: [1, 2, 4],
+      favorite_bands: [2, 8]
+    })
+
+    user2 = User.build({
+      username: 'correctUserwertyuiopasdfgghj',
+      favorite_bands: [2, 'garbagedata']
+    })
+  })
+
+  describe('username ---', () => {
+
+
+      // creatingItem1.description = 'Wow!'
+      // return creatingItem1.validate()
+      // .then(result => {
+      //   expect(result).to.be.an.instanceOf(Error)
+      //   expect(result.message).to.contain('content of comments must be at least 10 characters')
+      // })
+
+
+
+    it('resolves true if under 25 characters', () => {
+      return user1.validate()
+      .then((result) => {
+        expect(result).to.not.be.an.instanceOf(Error)
+      })
+    })
+
+    it('resolves false if greater that 25 characters', () => {
+      return user2.validate()
+      .then((result) => {
+        expect(result).to.be.an.instanceOf(Error)
+        // expect(result.message).to.contain()
+      })
+    })
+  })
+
+
+  describe('isVendor ---', () => {
+
+    it('resolves true if set to true', () => {
+      return user1.validate()
+      .then(() => {
+        expect(user1.is_vendor).to.be.equal(true)
+      })
+    })
+
+    it('resolves false if not set', () => {
+      return user2.validate()
+      .then(() => {
+        expect(user2.is_vendor).to.be.equal(false)
+      })
+    })
+  })
+
+  describe('isAdmin ---', () => {
+
+    it('resolves true if set to true', () => {
+      return user1.validate()
+      .then(() => {
+        expect(user1.is_vendor).to.be.equal(true)
+      })
+    })
+
+    it('resolves false if not set', () => {
+      return user2.validate()
+      .then(() => {
+        expect(user2.is_vendor).to.be.equal(false)
+      })
+    })
+  })
+
+  describe('endorsedBy ---', () => {
+
+    it('resolves true if contains integers', () => {
+      return user1.save()
+      .catch((result) => {
+        console.log(result)
+        expect(result).to.be.an.instanceOf(Error)
+      })
+    })
+
+    it('sets to empty array if not set', () => {
+      return user2.validate()
+      .then(() => {
+        expect(user2.endorsed_by).to.deep.equal([])
+      })
+    })
+  })
+
+  describe.only('favorites ---', () => {
+
+    describe('manual validation for users and bands', () => {
+
+      it('resolves true if under 3 and false if over', () => {
+        return user1.validate()
+        .then(() => {
+          expect(user1.favorite_users.length).to.be.below(4)
+          expect(user1.favorite_bands.length).to.be.below(4)
+        })
+      })
+
+      it('sets to empty array if not set', () => {
+        return user2.validate()
+        .then(() => {
+          expect(user2.favorite_users).to.deep.equal([])
+        })
+      })
+    })
+
+    // describe('updateFavorites instance method', () => {
+
+    //   it('changes the favorite users', () => {
+
+    //   })
+
+    //   it('changes the favorite bands', () => {
+
+    //   })
+
+    //   it('does not update if input is not valid', () => {
+
+    //   })
+    // })
   })
 })
