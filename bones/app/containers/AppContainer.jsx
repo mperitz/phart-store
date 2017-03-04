05d997134c67d553c4a51bb020e5b024b96f7d83
Mike Peritz <mike.peritz@gmail.com>
2017-03-04 17:28:14 -0500

    styled navbar and sidebar

--- bones/app/containers/AppContainer.jsx
+++ bones/app/containers/AppContainer.jsx
@@ -6,11 +6,11 @@ import NavbarContainer from '../containers/NavbarContainer'
 export default function ({ children }) {
   return (
     <div id="main" className="container-fluid">
-      <NavbarContainer />
       <div className="col-xs-2">
         <SidebarContainer />
       </div>
       <div className="col-xs-10">
+        <NavbarContainer />
         { children }
       </div>
 
